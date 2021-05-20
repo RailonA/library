1bfa261199567d85c8753a644231b32fa667f8e8
RailonA <railonacosta@gmail.com>
2021-05-20 10:46:07 -0400

    Add all files

---
+++ script/main.js
@@ -0,0 +1,10 @@
+/* eslint-disable no-underscore-dangle */
+/* eslint-disable import/extensions */
+/* eslint-disable no-use-before-define */
+import {
+    getDomElement,
+    setInnerHTML,
+    resetValue,
+    getAllElementsOfType,
+    handleEvent,
+  } from './dom.js';
