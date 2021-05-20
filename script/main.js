61ab8b23c4d44c1e51b7a386d042310694b1e706
RailonA <railonacosta@gmail.com>
2021-05-20 13:32:39 -0400

    Fix book display on html index

--- script/main.js
+++ script/main.js
@@ -3,26 +3,28 @@
   /* eslint-disable import/extensions */
   /* eslint-disable no-use-before-define */
 
-  import {
-    getDomElement,
-    setInnerHTML,
-    resetValue,
-    getAllElementsOfType,
-    handleEvent,
-    } from './dom.js'; 
+  // import {
+  //   getDomElement,
+  //   setInnerHTML,
+  //   resetValue,
+  //   getAllElementsOfType,
+  //   handleEvent,
+  //   } from './dom.js'; 
   
-  let myLibrary = [
-    {
-      author: "Rashad",
-      title: "random",
-      pages: 23
-    },
+  let body = document.getElementsByTagName("body");
 
-    {
-      author: "Railon",
-      title: "random",
-      pages: 45
-    }
+  let myLibrary = [
+    // {
+    //   author: "Rashad",
+    //   title: "random",
+    //   pages: 23
+    // },
+
+    // {
+    //   author: "Railon",
+    //   title: "random",
+    //   pages: 45
+    // }
   ]
 
   function Book(author, title, pages){
@@ -38,12 +40,35 @@
       document.getElementById('title').value,
       document.getElementById('pages').value
     )
-    myLibrary.unshift(newBook)
+    myLibrary.push(newBook)
     document.querySelector('form').reset()
     console.log(myLibrary)
   }
 
 
+
+
+ // Get the app element
+var libraryList = document.createElement("div");
+
+libraryList.innerHTML = myLibrary.map(function (book) {
+	return `<div class="">
+  <img class="card-img-top" src="https://nypost.com/wp-content/uploads/sites/2/2019/03/old-library-book.jpg?quality=80&strip=all" alt="Card image cap">
+    <h5>${book.author}</h5>
+    <h5>${book.title}</h5>
+    <h5>${book.pages}</h5>
+    </div>`;
+}).join('') ;
+
+
+  
+
+
+
+  document.body.appendChild(libraryList);
+
+
+ 
     document.getElementById("submit").addEventListener('click', addBookToLibrary)
 
   
