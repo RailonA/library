4ae4eeab707f4bc5c920e34e9f93b4b7a32eb976
RailonA <railonacosta@gmail.com>
2021-05-24 16:05:56 -0400

    Add toggole to form

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,10 @@
 /* eslint-disable no-underscore-dangle */
 /* eslint-disable no-use-before-define */
 
+// const { formatters } = require("stylelint");
+
 const myLibrary = [];
-const EXISTING_DATA = JSON.parse(localStorage.getItem('libraryBooks')) || [];
+// const EXISTING_DATA = JSON.parse(localStorage.getItem('libraryBooks')) || [];
 
 function Book(author, title, pages, readStatus) {
   this.author = author;
@@ -10,6 +12,15 @@ function Book(author, title, pages, readStatus) {
   this.pages = pages;
   this.readStatus = readStatus;
 }
+
+const addBookForm = document.getElementById("addBookForm");
+const showForm = document.getElementById('showForm')
+
+function displayForm() {
+  addBookForm.classList.toggle("d-none")
+}
+
+formBtn.addEventListener("click", displayForm)
 
 function addBookToLibrary(event) {
   event.preventDefault();
@@ -22,19 +33,19 @@ function addBookToLibrary(event) {
 
   myLibrary.unshift(newBook);
   document.querySelector('form').reset();
-  EXISTING_DATA.push(newBook);
-  localStorage.setItem('libraryBooks', JSON.stringify(EXISTING_DATA));
-  window.location.reload();
+  // myLibrary.push(newBook);
+  // localStorage.setItem('libraryBooks', JSON.stringify(EXISTING_DATA));
+  // window.location.reload();
 }
 
 const libraryList = document.createElement('div');
 
-for (let i = 0; i < localStorage.length; i += 1) {
-  const key = localStorage.key(i);
-  const value = localStorage.getItem(key);
-  const rObject = JSON.parse(value);
+for (let i = 0; i < myLibrary.length; i += 1) {
+  // const key = localStorage.key(i);
+  // const value = localStorage.getItem(key);
+  // const rObject = JSON.parse(value);
 
-  libraryList.innerHTML = rObject.map((book) => `<div class="d-flex flex-column col-6 justify-content-center">
+  libraryList.innerHTML = myLibrary.map((book) => `<div class="d-flex flex-column col-6 justify-content-center">
       <img class="card-img-top " src="https://nypost.com/wp-content/uploads/sites/2/2019/03/old-library-book.jpg?quality=80&strip=all" alt="Card image cap">
       <div class="d-flex flex-column col-12 "> 
        <h5> Author: ${book.author}</h5>
@@ -54,14 +65,18 @@ for (let i = 0; i < localStorage.length; i += 1) {
 document.body.appendChild(libraryList);
 document.getElementById('submit').addEventListener('click', addBookToLibrary);
 
-const btn = document.querySelectorAll('.deleteBtn');
-btn.forEach((element, index) => {
-  element.addEventListener('click', () => {
-    for (let i = 0; i < EXISTING_DATA.length; i += 1) {
-      console.log(i);
-      EXISTING_DATA.splice(index, 1);
-      localStorage.setItem('libraryBooks', JSON.stringify(EXISTING_DATA));
-      window.location.reload();
-    }
-  });
-});
+
+
+
+
+// const btn = document.querySelectorAll('.deleteBtn');
+// btn.forEach((element, index) => {
+//   element.addEventListener('click', () => {
+//     for (let i = 0; i < EXISTING_DATA.length; i += 1) {
+//       console.log(i);
+//       EXISTING_DATA.splice(index, 1);
+//       localStorage.setItem('libraryBooks', JSON.stringify(EXISTING_DATA));
+//       window.location.reload();
+//     }
+//   });
+// });
