6a90e98ba45b1ec7a9a11125795c3dfae1d0c629
RailonA <railonacosta@gmail.com>
2021-05-27 11:50:02 -0400

    Add class

--- scripts/main.js
+++ scripts/main.js
@@ -5,11 +5,13 @@
 
 const myLibrary = [];
 
-function Book(author, title, pages, readStatus) {
-  this.author = author;
-  this.title = title;
-  this.pages = pages;
-  this.readStatus = readStatus;
+class Book {
+  constructor(author, title, pages, readStatus) {
+    this.author = author;
+    this.title = title;
+    this.pages = pages;
+    this.readStatus = readStatus;
+  }
 }
 
 const addBookForm = document.getElementById('addBookForm');
